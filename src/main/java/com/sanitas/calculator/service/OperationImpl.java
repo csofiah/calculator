a574bfec1c1b5e30c4a1a6bc10b81f8d5961c18f
Sofia Hernandez <csofiah>
2022-04-27 12:42:50 -0500

    test with tracer

--- src/main/java/com/sanitas/calculator/service/OperationImpl.java
+++ src/main/java/com/sanitas/calculator/service/OperationImpl.java
@@ -1,43 +1,38 @@
 package com.sanitas.calculator.service;
 
-import com.sanitas.calculator.exception.OperationException;
 import com.sanitas.calculator.model.OperationModel;
 import io.corp.calculator.TracerAPI;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class OperationImpl implements Operation{
+public class OperationImpl implements Operation {
 
     private final TracerAPI tracerAPI;
 
     @Override
     public OperationModel add(OperationModel operationModel) {
-        try {
-            var result = operationModel.getNumber1() + operationModel.getNumber2();
-            //tracerAPI.trace(result);
 
-            return operationModel.toBuilder()
-                    .result(result)
-                    .build();
-        } catch (Exception ex) {
-           throw OperationException.Type.ERROR_OPERATION_ADD.build(ex);
-        }
+        var result = operationModel.getNumber1() + operationModel.getNumber2();
+        tracerAPI.trace(result);
+
+        return operationModel.toBuilder()
+                .result(result)
+                .build();
 
     }
 
     @Override
     public OperationModel substract(OperationModel operationModel) {
-        try {
-            return operationModel.toBuilder()
-                    .result(operationModel.getNumber1() - operationModel.getNumber2())
-                    .build();
+        var result = operationModel.getNumber1() - operationModel.getNumber2();
+        tracerAPI.trace(result);
 
-        } catch (Exception ex) {
-            throw OperationException.Type.ERROR_OPERATION_SUBSTRACT.build(ex);
-        }
+        return operationModel.toBuilder()
+                .result(result)
+                .build();
+
+
     }
 
 }
